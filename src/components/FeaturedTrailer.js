5e0c279b1d2a12378cf618050f1a9ab5e2f030d7
Sandro Hallado <sandrohallado@yahoo.com>
2020-01-06 21:51:50 -0800

    Add a feature game trailer section in trailer collection section

---
+++ src/components/FeaturedTrailer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const FeaturedTrailer = () => (
+	<div>
+		<div>
+			<p>video of videogame trailer</p>
+			<div>
+				<h1>Video Game Title</h1>
+				<p>video game description</p>
+				<div>
+					<a href="#">Play</a>
+					<a href="#">+ My Trailer</a>
+					<a href="#">More Info</a>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default FeaturedTrailer;
--- src/components/TrailerCollection.js
+++ src/components/TrailerCollection.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import FeaturedTrailer from './FeaturedTrailer';
 import TrailerCategory from './TrailerCategory';
 
 const TrailerCollection = () => (
 	<div>
+		<TrailerCategory />
+		<TrailerCategory />
+		<FeaturedTrailer />
 		<TrailerCategory />
 		<TrailerCategory />
 	</div>
