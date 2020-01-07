1d7d733e64a24fd7d8599f8974e7e106f9ecf070
Sandro Hallado <sandrohallado@yahoo.com>
2020-01-06 21:53:28 -0800

    Add footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+	<div>
+		<p>this is will just be LLIC</p>
+	</div>
+);
+
+export default Footer;
--- src/components/VGTApp.js
+++ src/components/VGTApp.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import NavBar from './NavBar';
 import TrailerCollection from './TrailerCollection';
+import Footer from './Footer';
 
 const VGTApp = () => (
 	<div>
 		<NavBar />
 		<Header />
 		<TrailerCollection />
+		<Footer />
 	</div>
 );
 
