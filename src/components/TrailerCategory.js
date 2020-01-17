fe157209b8ea04b60273baf645b9bc635f137180
Sandro Hallado <sandrohallado@yahoo.com>
2020-01-16 17:18:32 -0800

    Add trailercategory component styling

--- src/components/TrailerCategory.js
+++ src/components/TrailerCategory.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Trailer from './Trailer';
 
 const TrailerCategory = () => (
-	<div>
-		<div>
-			<h3>Trailer Category</h3>
-			<p>- - - -</p>
+	<div className="trailer-category">
+		<div className="trailer-category__heading">
+			<h2 className="trailer-category__title">Trailer Category</h2>
+			<p className="trailer-category__status-bar">- - - -</p>
 		</div>
 		<Trailer />
 	</div>
