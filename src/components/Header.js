edeeefbe20efc1411b1bb632e13e686605d6f9bf
Sandro Hallado <sandrohallado@yahoo.com>
2020-01-16 16:04:05 -0800

    Add header section styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
 const Header = () => (
-	<header>
-		<h1>Video Game Title</h1>
-		<p>video game description</p>
-		<div>
-			<div>
-				<a href="#">Play</a>
-				<a href="#">+ My Trailer</a>
-				<a href="#">More Info</a>
+	<header className="header">
+		<div className="header__info">
+			<h1 className="header__title">Halo Reach</h1>
+			<p className="header__description">
+				Halo: Reach is a 2010 first-person shooter developed by Bungie and published by Microsoft Game Studios, originally for the Xbox 360
+			</p>
+		</div>
+		<div className="videogame-content">
+			<div className="videogame-content__links">
+				<a href="#" className="videogame-content__link">
+					<ion-icon name="play"></ion-icon>
+					<p>Play</p>
+				</a>
+				<a href="#" className="videogame-content__link">
+					<ion-icon name="add"></ion-icon>
+					<p>My Trailer</p>
+				</a>
+				<a href="#" className="videogame-content__link">
+					<ion-icon name="information-circle-outline"></ion-icon>
+					<p>More Info</p>
+				</a>
 			</div>
-			<p>video game rating</p>
+			<p className="videogame-content__rating">Rated M</p>
 		</div>
 	</header>
 );
