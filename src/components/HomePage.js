5ab52fdddd814df39df52f815908a0a15b2bc05a
Sandro Hallado <sandrohallado@yahoo.com>
2020-01-22 22:18:39 -0800

    Setup routing for login and signup page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,22 +1,25 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const HomePage = () => (
-	<div className="home">
-		<div className="home__container-one">
-			<img src="img/logo.png" alt="company logo" className="home__logo"></img>
-			<a href="#" className="home__signin-link">Sign In</a>
-		</div>
-		<div className="home__container-two">
-			<h1 className="home__primary-heading">
-				Unlimited video game trailers and watch anytime
-			</h1>
-			<h2 className="home__secondary-heading">
-				Keep up to date with upcoming video games
-			</h2>
-			<a href="#" className="home__signup-link">Sign Up</a>
-		</div>
-	</div>
+  <div className="home">
+    <div className="home__container-one">
+      <img src="img/logo.png" alt="company logo" className="home__logo"></img>
+      <Link to="/login" className="home__signin-link">
+        Sign In
+      </Link>
+    </div>
+    <div className="home__container-two">
+      <h1 className="home__primary-heading">
+        Unlimited video game trailers and watch anytime
+      </h1>
+      <h2 className="home__secondary-heading">
+        Keep up to date with upcoming video games
+      </h2>
+      <a href="#">Sign Up</a>
+      <Link to="/signup" className="home__signup-link">Sign Up</Link>
+    </div>
+  </div>
 );
 
 export default HomePage;
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -3,16 +3,20 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AccountPage from '../components/AccountPage';
 import BrowsePage from '../components/BrowsePage';
 import HomePage from '../components/HomePage';
+import LoginPage from '../components/LoginPage';
 import MyListPage from '../components/MyListPage';
 import NintendoPage from '../components/NintendoPage';
 import PCPage from '../components/PCPage';
 import PlayStationPage from '../components/PlayStationPage';
+import SignUpPage from '../components/SignUpPage';
 import XboxPage from '../components/XboxPage';
 
 const AppRouter = () => (
 	<BrowserRouter>
 		<Switch>
 			<Route path="/" component={HomePage} exact={true} />
+			<Route path="/login" component={LoginPage} />
+			<Route path="/signup" component={SignUpPage} />
 			<Route path="/browse" component={BrowsePage} />
 			<Route path="/browse/nintendo" component={NintendoPage} />
 			<Route path="/browse/playstation" component={PlayStationPage} />
