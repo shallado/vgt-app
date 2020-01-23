001d277f2a45f71d8ec3a913b354aa0b1013a960
Sandro Hallado <sandrohallado@yahoo.com>
2020-01-22 21:08:49 -0800

    Setup routing for account and homepage

--- src/components/AccountMenu.js
+++ src/components/AccountMenu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AccountMenu = () => (
   <div className="account-menu">
@@ -8,16 +9,20 @@ const AccountMenu = () => (
         alt="profile"
         className="account-menu__profile-img"
       ></img>
-			<div className="account-menu__icon">
-      	<ion-icon name="arrow-dropdown"></ion-icon>
-			</div>
+      <div className="account-menu__icon">
+        <ion-icon name="arrow-dropdown"></ion-icon>
+      </div>
     </div>
     <div className="account-menu__content-container">
-			<div className="account-menu__icon--alt">
-      	<ion-icon name="arrow-dropup"></ion-icon>
-			</div>
-      <a href="/YourAccount" className="account-menu__profile">Manage Profile</a>
-      <a href="/" className="account-menu__sign-out">Sign out of VGT</a>
+      <div className="account-menu__icon--alt">
+        <ion-icon name="arrow-dropup"></ion-icon>
+      </div>
+      <Link to="/account" className="account-menu__profile">
+        Manage Profile
+      </Link>
+      <Link to="/" className="account-menu__sign-out">
+        Sign out of VGT
+      </Link>
     </div>
   </div>
 );
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AccountPage from '../components/AccountPage';
 import BrowsePage from '../components/BrowsePage';
+import HomePage from '../components/HomePage';
+import MyListPage from '../components/MyListPage';
 import NintendoPage from '../components/NintendoPage';
+import PCPage from '../components/PCPage';
 import PlayStationPage from '../components/PlayStationPage';
 import XboxPage from '../components/XboxPage';
-import PCPage from '../components/PCPage';
-import MyListPage from '../components/MyListPage';
 
 const AppRouter = () => (
 	<BrowserRouter>
 		<Switch>
-			<Route path="/browse" component={BrowsePage} exact={true} />
+			<Route path="/" component={HomePage} exact={true} />
+			<Route path="/browse" component={BrowsePage} />
 			<Route path="/browse/nintendo" component={NintendoPage} />
 			<Route path="/browse/playstation" component={PlayStationPage} />
 			<Route path="/browse/xbox" component={XboxPage} />
 			<Route path="/browse/pc" component={PCPage} />
 			<Route path="/my-list" component={MyListPage} />
+			<Route path="/account" component={AccountPage} />
 		</Switch>
 	</BrowserRouter>
 );
