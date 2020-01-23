06e4b332f52d75fb4956d37539c1c198157b50e6
Sandro Hallado <sandrohallado@yahoo.com>
2020-01-22 22:08:47 -0800

    Add homepage content and styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+
 const HomePage = () => (
-	<div>
-		HomePage
+	<div className="home">
+		<div className="home__container-one">
+			<img src="img/logo.png" alt="company logo" className="home__logo"></img>
+			<a href="#" className="home__signin-link">Sign In</a>
+		</div>
+		<div className="home__container-two">
+			<h1 className="home__primary-heading">
+				Unlimited video game trailers and watch anytime
+			</h1>
+			<h2 className="home__secondary-heading">
+				Keep up to date with upcoming video games
+			</h2>
+			<a href="#" className="home__signup-link">Sign Up</a>
+		</div>
 	</div>
 );
 
