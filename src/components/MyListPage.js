94ed16dc685598729779a469cb5283bbfd93e365
Sandro Hallado <sandrohallado@yahoo.com>
2020-01-23 20:26:55 -0800

    Add my list page content and styling

--- src/components/MyListPage.js
+++ src/components/MyListPage.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import NavBar from './NavBar';
 
 const MyListPage = () => (
-	<div>
-		My List Page
+	<div className="listpage">
+		<NavBar />
+		<div className="listpage__header">
+			<h1>My List</h1>
+			<div>
+				<label>Sort By</label>
+				<select>
+					<option value="year released">Year Released</option>
+					<option value="a-z">A-Z</option>
+					<option value="z-a">Z-A</option>
+				</select>
+			</div>
+		</div>
 	</div>
 );
 
