0c9d0fde4c8a29bec09d4d5b01b98ee815b96ed9
Sandro Hallado <sandrohallado@yahoo.com>
2020-01-23 16:11:32 -0800

    Add loginform component and styling

---
+++ src/components/LogInForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const LogInForm = () => (
+	<form className="login-form">
+		<div className="input-field">
+			<div className="input-field__label-container">
+				<label htmlFor="email">Email</label>
+			</div>
+			<input type="email" id="email" className="input-field__input" />
+		</div>
+		<div className="input-field">
+			<div className="input-field__label-container">
+				<label htmlFor="password">Password</label>
+			</div>
+			<input type="password" id="password" className="input-field__input" />
+		</div>
+		<button>Log In</button>
+	</form>
+);
+
+export default LogInForm;
