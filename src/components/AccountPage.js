5b39d2e9458636b4027be42f5f3643ef755ab056
Sandro Hallado <sandrohallado@yahoo.com>
2020-01-23 18:16:52 -0800

    Add account page and styling

--- src/components/AccountPage.js
+++ src/components/AccountPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import AccountMenu from './AccountMenu';
 
 const AccountPage = () => (
-	<div>
-		AccountPage
-	</div>
+  <div className="account">
+    <div className="account__header">
+      <img src="img/logo.png" alt="company logo" className="home__logo"></img>
+			<AccountMenu />
+    </div>
+    <div className="account__container">
+      <h1 className="account__title">Your Account</h1>
+      <div className="personal-info">
+        <div className="personal-info__header">
+          <h2 className="personal-info__title">Personal Info</h2>
+          <button className="update-info-btn">Update Info</button>
+        </div>
+        <ul className="personal-info__details">
+          <li>Ryan Bang</li>
+          <li>01/10/20</li>
+          <li>Male</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 );
 
 export default AccountPage;
